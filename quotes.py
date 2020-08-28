a7af44d78713ab1035db34b450405d90d577a038
Ben <ben@stix.site>
2020-08-28 15:12:37 -0400

    quotes works very well now.
    still can't delete quotes. still can't list quotes. will do this LATER

--- quotes.py
+++ quotes.py
@@ -1,5 +1,6 @@
 from models import Quote, Session
 from discord.ext import commands
+import discord
 from sqlalchemy import desc
 import random
 import typing
@@ -11,15 +12,23 @@ class Quotes(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def quote(self, ctx, number: typing.Optional[int]):
+    async def quote(self, ctx, arg: typing.Union[int, discord.Member, None]):
         q = None
         server = ctx.guild.id
-        if number:
-            q = session.query(Quote).filter(Quote.number == number, Quote.server == server).one()
-            if q is none: return
+        quotes = session.query(Quote).filter(Quote.server == server)
+        if arg:
+            if type(arg) is int:
+                q = quotes.filter(Quote.number == arg).one()
+            elif type(arg) is discord.Member:
+                q = random.choice(quotes.filter(Quote.author == arg.id).all())
         else:
-            q = random.choice(session.query(Quote).filter(Quote.server==server).all())
-        await ctx.send(q.message)
+            q = random.choice(quotes.all())
+        if q is None: return
+        # get the user
+        author = await self.bot.fetch_user(q.author)
+        r = f'"{q.message}"\n—{author.name} (Quote #{q.number})'
+        
+        await ctx.send(r)
     
     @commands.command()
     async def addquote(self, ctx, message_id):
@@ -35,7 +44,7 @@ class Quotes(commands.Cog):
             q.number = highest.number+1 if highest else 1
             session.add(q)
             session.commit()
-            await ctx.send("added")
+            await ctx.send(f'added. it\'s quote {q.number}')
 
 
 
