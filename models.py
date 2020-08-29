4e192c60dff48b3a15187d8c3b0bc18786b891a3
Ben Johnson-Staub <benjohnsonstaub@gmail.com>
2020-08-29 19:03:23 -0400

    add new models for servers and users

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.dialects.mysql import BIGINT
 engine = create_engine('sqlite:///db.sqlite3')
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
@@ -17,3 +18,17 @@ class Quote(Base):
     added_by = Column(String)
     number = Column(Integer)
 
+class Server(Base):
+    __tablename__ = 'servers'
+    id = Column(Integer, primary_key=True)
+    server_id = Column(BIGINT(unsigned=True), unique=True)
+    
+    # if your server name has more than 100 chars tough luck
+    name = Column(String(100))
+    muted_role_id = Column(Integer)
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(BIGINT(unsigned=True), unique=True)
+    server = Column(Integer, ForeignKey('servers.id'))
