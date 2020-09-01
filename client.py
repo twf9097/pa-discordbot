d2e501bc9e0ea95c0dd3c0a8fede4541bbd7f04f
Ben Johnson-Staub <benjohnsonstaub@gmail.com>
2020-08-31 20:34:22 -0400

    add cog to client

--- client.py
+++ client.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 from quotes import Quotes
+from mutes import Mutes
+
 client = commands.Bot(command_prefix="*")
 
 
@@ -9,3 +11,4 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 client.add_cog(Quotes(client))
+client.add_cog(Mutes(client))
